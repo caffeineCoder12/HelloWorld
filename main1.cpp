eece6de7d30a282af1d63ee0af4afd9ed3f9e185
caffeineCoder12 <142076630+caffeineCoder12@users.noreply.github.com>
2023-10-28 21:18:34 +0530

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+
+/*
+        This is a block comment. It encloses a number of lines.
+*/
+
+int main(){
+    //Printing Hello world
+    std::cout<<"Hello World!"<<std::endl<<"I am practicing commenting on a C++ program.";
+    return 0;
+    //Program ends here
+}
